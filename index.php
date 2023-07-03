6ede69b0f68a667949b8966cd94aec384b037b3d
Jacob <45181318+jcbgrub@users.noreply.github.com>
2023-07-03 09:21:34 +0200

    Development (#1)
    * adding static files
    
    * adding favicon
    
    ---------
    
    Co-authored-by: Jacob Grüß <jgruess@jgruess.local>

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>&#65279;</title>
+    <!-- <link rel="shortcut icon" href="static/favicon_dark.png" type="image/x-icon"/> -->
+    <link rel="icon" href="static/favicon_dark.png" media="(prefers-color-scheme: light)" sizes="96x96">
+    <link rel="icon" href="static/favicon_light.png" media="(prefers-color-scheme: dark)" sizes="96x96">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-image: url('https://media.giphy.com/media/YRcXl6VfNhCorklI0R/giphy.gif');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        #content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        #svg-image {
+            width: 300px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <div id="content">
+        <svg id="svg-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200">
+            <image xlink:href="static/logo.svg" width="300" height="200" />
+        </svg>
+    </div>
+</body>
+</html>
